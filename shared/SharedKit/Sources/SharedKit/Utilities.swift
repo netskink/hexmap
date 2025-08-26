db3fc4d58121ba72feae8a5340abfaa00499bc67
john davis <davis@doom.lan>
2025-08-26 08:14:48 -0400

    examined how to work with pretty name

--- shared/SharedKit/Sources/SharedKit/Utilities.swift
+++ shared/SharedKit/Sources/SharedKit/Utilities.swift
@@ -18,6 +18,23 @@ public enum Log {
 }
 
 public struct AppInfo {
+//    What it does step by step:
+//    1.    Takes a bundle (default: .main)
+//      By default, it uses the main app bundle, but you can pass another Bundle if you want.
+//    2.    Looks for CFBundleDisplayName in the bundle’s Info.plist
+//      This is the localized, user-facing name of the app (what appears under the app icon on iOS home screen).
+//      See target->info-> Bundle Display Name = "not defined" proceed to step 3
+//    3.    Falls back to CFBundleName if CFBundleDisplayName isn’t set
+//      This is the internal short name of the bundle, often the project name.
+//      See target->info->BundleName = $(PRODUCT_NAME), eg "AppTwo"
+//    4.    Falls back to "App" if neither is available
+//      Provides a safe default string instead of returning nil.
+//
+//   swift a ?? b  nil-calescing operation
+//   a ?? b is a unless a is nil.  In that case the value is b.
+//
+//   In this case, its a ?? b ?? c
+    
     public static func prettyName(_ bundle: Bundle = .main) -> String {
         bundle.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String
         ?? bundle.object(forInfoDictionaryKey: "CFBundleName") as? String
